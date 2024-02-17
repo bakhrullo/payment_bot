87171bf9b41848f22d5b7b674335040fc19f0fed
bakhrullo <81313486+bakhrullo@users.noreply.github.com>
2024-02-17 13:51:25 +0500

    fiz

--- tgbot/handlers/user.py
+++ tgbot/handlers/user.py
@@ -9,13 +9,25 @@ from tgbot.misc.states import UserState
 
 
 async def user_start(m: Message):
-    await m.answer("Assalomu alaykum 😊\nSiz Ilhom Begimqulovning rasmiy to'lov botiga tashrif buyurdingiz!"
-                   "\nQaysi kursizmizga yozilmoqchisiz ?\n"
+    await m.answer("Assalomu alaykum 😊\nSiz Ilhom Begimqulovning rasmiy to'lov botiga tashrif buyurdingiz!\nIltimos, ismingizni kiriting!")
+    await UserState.get_name.set()
+
+
+async def get_name(m: Message, state: FSMContext):
+    await state.update_data(name=m.text)
+    await m.answer("☎️ Iltimos, telefon nomeringizni kiriting yoki pastdagi 'Kontaktni ulashish' tugmachasini bosing!",
+                   reply_markup=contact_btn)
+    await UserState.next()
+
+
+async def get_number(m: Message, state: FSMContext, config: Config):
+    phone = m.contact.phone_number if m.content_type == "contact" else m.text
+    await state.update_data(phone=phone)
+    await m.answer("Qaysi kursizmizga yozilmoqchisiz ?\n"
                    "1 STANDART <s>4 277 000</s>  3 277 000\n"
                    "2 PREMIUM  <s>4 977 000</s>  3 777 000\n"
                    "3 VIP  <s>50 000 000</s>  40 000 000", reply_markup=payments_kb)
-    await UserState.get_course.set()
-
+    
 
 async def get_course(c: CallbackQuery, state: FSMContext):
     await state.update_data(course=c.data)
@@ -28,30 +40,18 @@ async def get_course(c: CallbackQuery, state: FSMContext):
 
 
 async def get_photo(m: Message, state: FSMContext):
-    await state.update_data(photo=m.photo[-1].file_id)
-    await m.answer("Iltimos, ismingizni kiriting!")
-    await UserState.next()
-
-
-async def get_name(m: Message, state: FSMContext):
-    await state.update_data(name=m.text)
-    await m.answer("☎️ Iltimos, telefon nomeringizni kiriting yoki pastdagi 'Kontaktni ulashish' tugmachasini bosing!",
-                   reply_markup=contact_btn)
-    await UserState.next()
-
-
-async def get_number(m: Message, state: FSMContext, config: Config):
-    await m.answer("Operatorlarimiz tez orada sizga aloqaga chiqishadi! 😊️", reply_markup=remove_btn)
-    phone = m.contact.phone_number if m.content_type == "contact" else m.text
+    await m.answer("Operatorlarimiz tez orada sizga aloqaga chiqishadi! 😊️")
     data = await state.get_data()
-    await m.bot.send_photo(config.tg_bot.group_id, data['photo'],
+    await m.bot.send_photo(config.tg_bot.group_id, m.photo[-1].file_id,
                            caption=f"👨 Ism: {data['name']}\n📱 Raqam: {phone}\n💸 Kurs: {data['course']}")
+
+    
 
 
 def register_user(dp: Dispatcher):
     dp.register_message_handler(user_start, commands=["start"], state="*", chat_type="private")
-    dp.register_callback_query_handler(get_course, state=UserState.get_course, chat_type="private")
-    dp.register_message_handler(get_photo, state=UserState.get_photo, content_types=['photo'], chat_type="private")
     dp.register_message_handler(get_name, state=UserState.get_name, chat_type="private")
     dp.register_message_handler(get_number, state=UserState.get_number, content_types=['text', 'contact'],
+    dp.register_callback_query_handler(get_course, state=UserState.get_course, chat_type="private")
+    dp.register_message_handler(get_photo, state=UserState.get_photo, content_types=['photo'], chat_type="private")
                                 chat_type="private")
--- tgbot/misc/states.py
+++ tgbot/misc/states.py
@@ -2,7 +2,7 @@ from aiogram.dispatcher.filters.state import StatesGroup, State
 
 
 class UserState(StatesGroup):
-    get_course = State()
-    get_photo = State()
     get_name = State()
     get_number = State()
+    get_course = State()
+    get_photo = State()
